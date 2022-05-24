83adea658e4326f075d31657459acd8cf946f53b
unknown <debadyuti.karmakar@gmail.com>
2022-05-24 22:16:39 +0530

    23_5

---
+++ JS/datatypeandvar/JS/index.js
@@ -0,0 +1,13 @@
+/*var a = 10;
+var a = 20;
+console.log(a);
+*/
+/*
+let a =10;
+a =20;
+console.log(a);
+*/
+const a = 10;
+a=5;
+console.log(a);
+let k = true;
---
+++ JS/loop/JS/nested.js
@@ -0,0 +1,5 @@
+for(let i=1;i<5;i++){
+    for(let j=1;j<5;j++){
+        console.log(j);
+    }
+}
